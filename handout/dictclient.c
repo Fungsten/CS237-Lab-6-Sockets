7c128006aec1ef6a32db9265c1551252ff0fad42
Will Fung <wf2@williams.edu>
2018-05-03 14:46:10 -0400

    done part a?

--- handout/dictclient.c
+++ handout/dictclient.c
@@ -1,12 +1,15 @@
-/* 
+/*
  * dictclient.c - A simple dictionary client
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define BUFSIZE 1024
 
@@ -17,7 +20,7 @@ void error(char *msg) {
 }
 
 /*
- * This function returns a pointer to a substring of 
+ * This function returns a pointer to a substring of
  * the original string str with white leading and trailing
  * white space removed.  It is used by find_synonym().
  */
@@ -48,8 +51,8 @@ char *trim_whitespace(char *str) {
 char *find_synonym(char* fromserver, int fd) {
   char *syn, *trimsyn, *save;
   int n;
-  
-  /* Read the server's reply.  Continue reading until 
+
+  /* Read the server's reply.  Continue reading until
    * line beginning with 151 is found.
    */
   bzero(fromserver, BUFSIZE);
@@ -58,21 +61,21 @@ char *find_synonym(char* fromserver, int fd) {
       error("ERROR reading from socket");
       break;
     }
-    
+
     //found a synonym!
     if(strstr(fromserver, "151") != NULL) break;
-    
+
     //no synonym found
     if(strstr(fromserver, "552") != NULL) return NULL;
-    
+
     bzero(fromserver, BUFSIZE);
   }
-  
+
   //Parse result. Find first synonym
   syn = strtok_r(fromserver, ",", &save);
   syn = strtok_r(NULL, ":", &save);
   syn = strtok_r(NULL, ",", &save);
-  
+
   //Remove leading and trailing white space
   trimsyn = trim_whitespace(syn);
 
@@ -91,13 +94,63 @@ int setup_socket() {
 
 int main(int argc, char **argv) {
   char *trimmed_syn="";
-  
+  // char word[50];
+  int sockfd, n, status;
+  struct addrinfo hints;
+  struct addrinfo *servinfo;
+  char *hostname, *res, *portno;
+  char buf[BUFSIZE];
+  char prefix[100] = "define moby-thesaurus ";
+
+  hostname = "www.dict.org";
+  portno = "2628";
+
+  memset(&hints, 0, sizeof(hints)); // make sure the struct is empty
+  hints.ai_family = AF_INET;        // guess that we're using IPv4
+  hints.ai_socktype = SOCK_STREAM; // guess that we're using TCP
+
+  status = getaddrinfo(hostname, portno, &hints, &servinfo);
+  if (status)  error("ERROR, no such host");
+
+  sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+  if (sockfd < 0) error("ERROR opening socket");
+
+  if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+   error("ERROR connecting");
+
+
   /* get word from stdin */
   printf("Please enter word: ");
 
+  bzero(buf, BUFSIZE);
+  res = fgets(buf, BUFSIZE, stdin);
+  printf("%s",buf);
+  strcat(prefix, buf);
+  printf("%s", prefix);
+  if (res == NULL) error("ERROR reading from stdin\n");
+
+  printf("got past reading from stdin\n");
+  // trimmed_syn = find_synonym(hostname, res);
   /* print result to stdout */
+  /* Writing to the server */
+  n = write(sockfd, prefix, strlen(prefix));
+  printf("n: %d",n);
+  if (n < 0) error("Error Writing to socket");
+  printf("got past writing to socket\n");
+
+  /* read: print the server's reply */
+  bzero(buf, BUFSIZE);
+  n = read(sockfd, buf, BUFSIZE);
+  if (n < 0) error("ERROR reading from socket");
+  //printf("Echo from server: %s", buf);
+  printf("got past reading from socket\n");
+
+  trimmed_syn = find_synonym(buf, sockfd);
   printf("Synonym: %s \n", trimmed_syn);
-  
+  printf("finished\n");
+
+  close(sockfd);
+  freeaddrinfo(servinfo);
+
   return 0;
 }
-
