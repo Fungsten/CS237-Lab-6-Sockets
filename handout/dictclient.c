c7f1f452a0a9c2f58e99ff2e2c9de29951e7e7c6
Will Fung <wf2@williams.edu>
2018-05-15 14:30:54 -0400

    completed ver.

--- handout/dictclient.c
+++ handout/dictclient.c
@@ -11,8 +11,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define BUFSIZE 1024
+
+void *thread_func(void *vargp);
 
 /* error - wrapper for perror */
 void error(char *msg) {
@@ -94,12 +97,71 @@ int setup_socket() {
 
 
 int main(int argc, char **argv) {
+  char *res;
+  char buf[BUFSIZE];
+
+  /* get word from stdin */
+  printf("Please enter a phrase: ");
+  bzero(buf, BUFSIZE);
+  res = fgets(buf, BUFSIZE, stdin);
+  if (res == NULL) error("ERROR reading from stdin\n");
+  char *token;
+  char *word = res;
+  char *save;
+  int counter = 0;
+
+  pthread_t **tidarray = malloc(50 * sizeof(pthread_t*));
+  char **wordarray = malloc(50 * sizeof(char*));
+
+  while ((token = strtok_r(word, " ", &save))) {
+    word = NULL;
+
+    char *heapword = malloc(strlen(token) * sizeof(char));
+    pthread_t *tidtemp = malloc(sizeof(pthread_t));
+
+    strcpy(heapword, token);
+    wordarray[counter] = heapword;
+    tidarray[counter] = tidtemp;
+    //create new peer thread, call thread_func
+    if (pthread_create(tidarray[counter], NULL, thread_func, heapword) != 0) {
+      printf("error creating thread\n");
+      exit(1);
+    }
+    counter++;
+  }
+
+  //wait for peer thread to complete before exiting
+  for (int i = 0; i < counter; i++){
+    if (pthread_join(*(tidarray[i]), (void **)(&wordarray[i])) != 0) {
+      printf("error joining thread\n");
+      exit(1);
+    }
+
+  }
+  printf("New phrase:");
+
+  for (int i = 0; i < counter; i++){
+    printf(" %s", wordarray[i]);
+  }
+
+  printf("\n");
+  for (int i = counter; i > 0; i--) {
+    free(tidarray[i]);
+    free(wordarray[i]);
+  }
+  free(wordarray);
+  free(tidarray);
+  return 0;
+}
+
+
+/* thread routine */
+void *thread_func(void *vargp) {
   char *trimmed_syn="";
-  // char word[50];
   int sockfd, n, status;
   struct addrinfo hints;
   struct addrinfo *servinfo;
-  char *hostname, *res, *portno;
+  char *hostname, *portno;
   char buf[BUFSIZE];
   char prefix[100] = "define moby-thesaurus ";
 
@@ -120,30 +182,36 @@ int main(int argc, char **argv) {
    error("ERROR connecting");
 
 
-  /* get word from stdin */
-  printf("Please enter word: ");
+  // printf("%s\n", (char*) vargp);
+  char *word = (char*) vargp;
+  strcat(prefix, word);
+  strcat(prefix, "\n");
 
-  bzero(buf, BUFSIZE);
-  res = fgets(buf, BUFSIZE, stdin);
-  strcat(prefix, buf);
-  if (res == NULL) error("ERROR reading from stdin\n");
-
-  /* print result to stdout */
   /* Writing to the server */
   n = write(sockfd, prefix, strlen(prefix));
   if (n < 0) error("Error Writing to socket");
 
   /* read: print the server's reply */
-  bzero(buf, BUFSIZE);
-  n = read(sockfd, buf, BUFSIZE);
-  if (n < 0) error("ERROR reading from socket");
+  //bzero(buf, BUFSIZE);
+  //n = read(sockfd, buf, BUFSIZE);
+  //if (n < 0) error("ERROR reading from socket");
 
   trimmed_syn = find_synonym(buf, sockfd);
-  printf("Synonym: %s \n", trimmed_syn);
-  // printf("finished\n");
+  if (trimmed_syn == NULL) {
+    // printf("Synonym: %s\n", word);
+    trimmed_syn = word;
+  } else {
+    // printf("Synonym: %s \n", trimmed_syn);
+  }
 
   close(sockfd);
   freeaddrinfo(servinfo);
 
-  return 0;
+  // vargp = trimmed_syn
+  char *result = malloc(strlen(trimmed_syn) * sizeof(char));
+  strcpy(result, trimmed_syn);
+
+  // return 0;
+  free(word);
+  return result;
 }
